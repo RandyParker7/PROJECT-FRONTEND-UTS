74032afc71379d8cf5e3b37d211546923a39dda5
RandyParker7 <randyestevan887@gmail.com>
2024-10-11 07:40:07 +0700

    penambahan fitur auto slide

--- js/script.js
+++ js/script.js
@@ -13,16 +13,44 @@ function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
+    if (slides.length === 0) return;
+    
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+        slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    if (dots.length > 0) {
+        dots[slideIndex-1].className += " active";
+    }
+}
+
+// Fungsi untuk auto slide
+function autoSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (slides.length === 0) return; 
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex-1].style.display = "block";
+    if (dots.length > 0) {
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        dots[slideIndex-1].className += " active";
+    }
+    setTimeout(autoSlides, 5000); 
 }
 
 // Toggle
